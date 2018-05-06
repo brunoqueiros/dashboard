6de28dfc1e93a1e50238db2014a6ebe7d5e45f61
Bruno Queiros <brunoofq@gmail.com>
2018-05-06 19:14:12 +0200

    Create Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
+
+export default () => (
+  <FontAwesomeIcon
+    icon={faSpinner}
+    size="5x"
+    pulse
+  />
+);
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import classnames from 'classnames';
+import Loader from '../Loader';
 import './styles.css';
 
 export default ({
   children,
+  lastUpdated,
   noPadding = false,
+  loading = false,
   title = '',
   col = 1,
   row = 1
@@ -14,7 +17,8 @@ export default ({
   })}>
     {title && <span className="widget-title">{title}</span>}
     <span className="widget-content">
-      {children}
+      {loading ? <Loader /> : children}
     </span>
+    {lastUpdated && <span className="text-small">Last updated at {lastUpdated}</span>}
   </div>
 );
