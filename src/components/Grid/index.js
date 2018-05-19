e3b01167d2687491a2506d85bb717f1236d1c223
Bruno Queiros <brunoofq@gmail.com>
2018-05-19 19:51:29 +0200

    CSS polyfill closes #1

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 
-export default ({ children, resolution = '720p' }) => (
-  <div className={`grid grid--${resolution}`}>
-    {children}
-  </div>
-);
+class Grid extends Component {
+  componentDidMount() {
+    this.setPollyfills();
+  }
+
+  setPollyfills() {
+    import ('../../utils/css-polyfills');
+    import ('../../utils/modernizr');
+  }
+
+  render() {
+    const { children, resolution } = this.props;
+
+    return (
+      <div className={`grid grid--${resolution}`}>
+        {children}
+      </div>
+    );
+  }
+}
+
+Grid.defaultProps = {
+  resolution: '720p'
+};
+
+Grid.propTypes = {
+  children: PropTypes.node,
+  resolution: PropTypes.oneOf(['720p', '1080p', '1440p', '2160p'])
+};
+
+export default Grid;
